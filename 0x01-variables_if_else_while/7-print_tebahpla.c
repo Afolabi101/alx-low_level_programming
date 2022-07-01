7131e48c601f0850e36652847b0c35a30a4a7f0b
Bakare Afolabi <bakareoafolabi@gmail.com>
2022-07-01 16:47:35 -0700

    commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - random number
+ *
+ * Description - main function
+ * Return:0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
