12cafa29031c97ad5a26a3423b474c7c9f0d2b8e
Bakare Afolabi <bakareoafolabi@gmail.com>
2022-07-01 15:49:46 -0700

    commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - random numbers
+ *
+ * Description - main function
+ * Return:0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 0;ch < 10; ch++)
+	{
+		printf("%i", ch);
+	}
+	printf("\n");
+	return (0);
+}
