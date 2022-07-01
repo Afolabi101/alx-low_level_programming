b98e89924d9d3c6bdce3b473df5cdc9c763a2c5a
Bakare Afolabi <bakareoafolabi@gmail.com>
2022-07-01 16:34:47 -0700

    commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - random number
+ *
+ * Description - main function
+ * Return:0
+ */
+int main(void)
+{
+	int c;
+
+	for ( c = 0 ; c < 10 ; c++)
+	{
+		putchar(c + '0');
+	}
+	putchar('\n');
+	return (0);
+}
