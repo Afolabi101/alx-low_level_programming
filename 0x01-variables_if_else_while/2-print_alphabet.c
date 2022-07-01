d953d26dc66e251bbda4937f10ab507573abe209
Bakare Afolabi <bakareoafolabi@gmail.com>
2022-07-01 12:10:23 -0700

    task22

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Return random number
+ *
+ * Description - main function
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		putchar(c);
+		putchar('\n');
+	}
+	return (0);
+}
