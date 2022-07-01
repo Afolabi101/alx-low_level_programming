5b6969b62c13617aab46ba80fe22528afddffc46
Bakare Afolabi <bakareoafolabi@gmail.com>
2022-07-01 13:41:12 -0700

    commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - return random
+ *
+ * Description - main function
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{	
+		putchar(ch);
+	}	
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{	
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
