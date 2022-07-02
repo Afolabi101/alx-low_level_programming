cd07dddcb3224f7b04f4c876ee8199adf27d4e57
Bakare Afolabi <bakareoafolabi@gmail.com>
2022-07-01 17:05:54 -0700

    commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - random number 
+ *
+ * Description - main function
+ * Return :0
+ */
+int main(void)
+{
+	char c;
+	char d;
+
+	for(c = 0; c <= 9; c++)
+	{
+		putchar(c + '0');
+	}
+	for(d = 'a'; d <= 'f'; d++)
+	{
+		putchar(d);
+	}
+	putchar('\n');
+	return (0);
+}
