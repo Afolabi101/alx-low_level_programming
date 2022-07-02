95373782ca262f4abc904032124e16fb0a3958a8
Bakare Afolabi <bakareoafolabi@gmail.com>
2022-07-01 17:37:14 -0700

    commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+/**
+ * main - random number
+ *
+ * Description - mainfunction
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
