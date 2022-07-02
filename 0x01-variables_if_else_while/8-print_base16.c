a8678f3b8ab527de976fd57e1d3802c76ed65dc9
Bakare Afolabi <bakareoafolabi@gmail.com>
2022-07-01 17:23:38 -0700

    commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,13 +3,13 @@
  * main - description using putchar for hexadcimals
  *
  * Description - main function
- * Return :0
+ * Return: 0
  */
 int main(void)
 {
 	char c;
 
-	for (c = '0'; c <= '9'; c++)
+	for (c = 0; c <= 9; c++)
 	{
 		putchar(c + '0');
 	}
