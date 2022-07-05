655b3d9536116f930b4ae86ab6ae7dbe3fd27110
Bakare Afolabi <bakareoafolabi@gmail.com>
2022-07-05 08:48:10 -0700

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - returning function
+ *
+ * Return : 0
+ */
+
+int main(void)
+{
+	char putChar[8] = {'_','p','u','t','c','h','a','r'};
+	unsigned int c;
+
+	for (c = 0; c < sizeof(putChar); c++)
+	{
+		_putchar(putChar[c]);
+	}
+	putchar('\n');
+	return (0);
+}
